cbcffc5752122805350f312dcb340e6ea1fd733c
rohitvishin <rohitvishwakarma65@gmail.com>
2025-10-19 16:50:05 +0530

    accuracy and time reduced

--- src/location.ts
+++ src/location.ts
@@ -1,3 +1,4 @@
+import * as IntentLauncher from "expo-intent-launcher";
 import * as Location from "expo-location";
 import { Alert } from "react-native";
 import { LOCATION_TASK_NAME } from "../background/locationTask";
@@ -10,7 +11,7 @@ export async function ensureBackgroundTracking(){
     if (!isRegistered) {
       console.log("📍 Restarting background tracking...");
       await Location.startLocationUpdatesAsync(LOCATION_TASK_NAME, {
-          accuracy: Location.Accuracy.Low,
+          accuracy: Location.Accuracy.Balanced,
           timeInterval: 60000, // 1 minute
           distanceInterval: 50, // 50 m
           pausesUpdatesAutomatically: false,
@@ -31,18 +32,30 @@ export async function ensureBackgroundTracking(){
 export async function startLocationTracking() {
   try {
     // 1️⃣ Make sure permissions are granted
-    const { status: fg } = await Location.requestForegroundPermissionsAsync();
-    if (fg !== "granted") {
-      Alert.alert("Location permission not granted");
+    const fg = await Location.requestForegroundPermissionsAsync();
+    if (fg.status !== "granted") {
+      Alert.alert("Permission required", "Please allow location access to continue.");
       return;
     }
 
     const { status: bg } = await Location.requestBackgroundPermissionsAsync();
     if (bg !== "granted") {
-      Alert.alert("Background location denied");
+      Alert.alert(
+        "Background Access Needed",
+        "Please allow background location in settings for continuous tracking."
+      );
       return;
     }
-
+      try {
+        const pkg = "com.rohitvish.employeeapp";
+        await IntentLauncher.startActivityAsync(
+          IntentLauncher.ActivityAction.IGNORE_BATTERY_OPTIMIZATION_SETTINGS,
+          { data: `package:${pkg}` }
+        );
+        console.log("✅ Prompted user to disable battery optimization");
+      } catch (e) {
+        console.log("Battery optimization settings not available:", e);
+      }
     // 2️⃣ Check if already running
     const alreadyRunning = await Location.hasStartedLocationUpdatesAsync(
       LOCATION_TASK_NAME
@@ -55,7 +68,7 @@ export async function startLocationTracking() {
 
     // 3️⃣ Start updates
     await Location.startLocationUpdatesAsync(LOCATION_TASK_NAME, {
-      accuracy: Location.Accuracy.Low,
+      accuracy: Location.Accuracy.Balanced,
       timeInterval: 60000, // 1 minute
       distanceInterval: 50, // 50 m
       pausesUpdatesAutomatically: false,
